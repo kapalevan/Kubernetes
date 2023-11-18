6d246cad3d7525c99740a73abb944151403953cc
Levan Kapanadze <kapa@Levans-MacBook-Pro.local>
2023-11-18 19:58:23 +0400

    added datastore and network parameters into variables file

--- k8s/multi-node-ha-cluster/api-load-balancer/terraform/variables.tf
+++ k8s/multi-node-ha-cluster/api-load-balancer/terraform/variables.tf
@@ -37,3 +37,13 @@ variable "datacenter_name" {
   description = "The name of the vSphere datacenter"
   type        = string
 }
+
+variable "datastore_name" {
+  description = "The name of the vSphere datastore"
+  type        = string
+}
+
+variable "network_name" {
+  description = "The name of the vSphere network"
+  type        = string
+}
