f57be25cdf8b67a1b979832e1263c43ee7e367f0
Levan Kapanadze <kapa@Levans-MacBook-Pro.local>
2023-11-19 20:48:32 +0400

    added worker node provisioning terraform configurations

---
+++ k8s/multi-node-ha-cluster/worker-nodes/terraform/variables.tf
@@ -0,0 +1,88 @@
+variable "worker_nodes" {
+  description = "List of worker nodes"
+  type = list(object({
+    name = string
+    ip   = string
+  }))
+}
+
+variable "vcenter_server" {
+  description = "vCenter server address"
+}
+
+variable "template_name" {
+  description = "OS Template name"
+}
+
+#variable "vm_ip" {
+#  description = "IP Address for the VM"
+#}
+
+#variable "vm_hostname" {
+#  description = "Hostname for the VM"
+#}
+
+variable "vm_domain" {
+  description = "Domain for the VM"
+}
+
+variable "vm_folder" {
+  description = "The destination folder for the VM in vSphere"
+  type        = string
+}
+
+variable "vsphere_user" {
+  description = "Username for vSphere"
+  type        = string
+}
+
+variable "vsphere_password" {
+  description = "Password for vSphere"
+  type        = string
+}
+
+variable "datacenter_name" {
+  description = "The name of the vSphere datacenter"
+  type        = string
+}
+
+variable "datastore_name" {
+  description = "The name of the vSphere datastore"
+  type        = string
+}
+
+variable "network_name" {
+  description = "The name of the vSphere network"
+  type        = string
+}
+
+#variable "resource_pool_name" {
+#  description = "The name of the vSphere resource pool"
+#  type        = string
+#}
+
+variable "cluster_name" {
+  description = "The name of the vSphere cluster"
+  type        = string
+}
+
+variable "ipv4_gateway" {
+  description = "The IPv4 gateway for the VM"
+  type        = string
+}
+
+variable "dns_servers" {
+  description = "List of DNS servers"
+  type        = list(string)
+}
+
+variable "ssh_username" {
+  description = "SSH username for the VM"
+  type        = string
+}
+
+variable "ssh_password" {
+  description = "SSH password for the VM"
+  type        = string
+  sensitive   = true  # Marks the variable as sensitive, preventing it from being displayed in logs
+}
