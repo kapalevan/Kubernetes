8596e22e62b504feb68634f3906c7c421aab7336
Levan Kapanadze <kapa@Levans-MacBook-Pro.local>
2023-11-28 17:59:12 +0400

    modified api-server terraform module

--- k8s/multi-node-ha-cluster/api-load-balancer/terraform/main.tf
+++ k8s/multi-node-ha-cluster/api-load-balancer/terraform/main.tf
@@ -63,7 +63,7 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks.0.size
+    size             = data.vsphere_compute_cluster.cluster
     eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
